fc0c1d16e8f3108b40783fadcc84ae295776800d
Renzo <renzol2@illinois.edu>
2021-10-27 00:31:56 -0500

    Add full responsiveness to 2-col layout

--- src/components/Utils/Hero/index.js
+++ src/components/Utils/Hero/index.js
@@ -9,20 +9,24 @@ const Hero = () => {
       <Container fluid>
         <Row>
           <Col lg={8} md={12} className={styles.headings}>
-            <h3 className="light-text">November 12-14, 2021</h3>
+            <Image
+              src={`${process.env.PUBLIC_URL}/img/fact-bird.png`}
+              className={styles.mobile_eagle}
+            />
+            <h3 className={`${styles.date} light-text`}>November 12-14, 2021</h3>
             <h1 className={styles.title}>
               We Unite <br /> Filipino-Americans.
             </h1>
             <p className={`${styles.description} light-text`}>
-              The leading Midwest Filipino-American conference for delegates to
-              discover more about the Filipino-American identity through
-              workshops, performance, and community.
+              FACT is the largest student-led Filipino-American conference in
+              the Midwest, promoting Filipino and Filipino-American culture
+              through workshops, panels, and performances.
             </p>
           </Col>
           <Col lg={4} md={12}>
             <Image
               src={`${process.env.PUBLIC_URL}/img/fact-bird.png`}
-              className={styles.bird}
+              className={styles.eagle}
             />
           </Col>
         </Row>
